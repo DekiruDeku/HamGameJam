2bdca14e8aa05572d8d37197081c1d8312a2e514
Anoke <rustamgta1011@gmail.com>
2023-04-03 00:06:07 +0300

    fix-1:

--- Assets/CameraFollowing.cs
+++ Assets/CameraFollowing.cs
@@ -3,53 +3,31 @@ using UnityEngine;
 
 public class CameraFollowing : MonoBehaviour
 {
-    public float dumping = 1.5f;
-    public Transform player;
-    public Vector2 offset = new Vector2(1f, 0.5f);
-    public bool isLeft;
-    public int lastX;
+    public Transform target; // The target to follow
+    public float damping = 5f; // The damping to apply to the camera movement
+    public float height = 0.5f; // The height of the camera above the target
+
+    private Vector3 _currentVelocity; // The current velocity of the camera movement
 
     private void Start()
     {
-        offset = new Vector2(Mathf.Abs(offset.x), offset.y);
-        FindPlayer(isLeft);
-    }
-
-    private void FindPlayer(bool playerIsLeft)
-    {
-        lastX = Mathf.RoundToInt(player.position.x);
-        if (playerIsLeft)
+        // If no target is specified, try to find one with the "Player" tag
+        if (!target)
         {
-            transform.position = new Vector3(player.position.x - offset.x, player.position.y + offset.y,
-                transform.position.z);
-        }
-        else
-        {
-            transform.position = new Vector3(player.position.x - offset.x, player.position.y + offset.y,
-                transform.position.z);
+            target = GameObject.FindGameObjectWithTag("Player")?.transform;
         }
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if (!player) return;
-        var currentX = Mathf.RoundToInt(player.position.x);
-        isLeft = currentX <= lastX;
+        // If no target is specified, return
+        if (!target) return;
 
-        lastX = Mathf.RoundToInt(player.position.x);
+        // Calculate the target position with the offset
+        var targetPosition = target.position + new Vector3(0f, height, target.position.z - 0.5f);
 
-        Vector3 target;
-
-        if (isLeft)
-        {
-            target = new Vector3(player.position.x - offset.x, player.position.y + offset.y, transform.position.z);
-        }
-        else
-        {
-            target = new Vector3(player.position.x - offset.x, player.position.y + offset.y, transform.position.z);
-        }
-
-        var currentPosition = Vector3.Lerp(transform.position, target, dumping * Time.deltaTime);
-        transform.position = currentPosition;
+        // Move the camera towards the target position with damping
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity,
+            damping * Time.fixedDeltaTime);
     }
 }
