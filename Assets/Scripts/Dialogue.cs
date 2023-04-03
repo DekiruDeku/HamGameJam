713ff9d868674bedee6d5f1daf55ae9c721c9f86
DekiruDeku <mikereaper@inbxo.ru>
2023-04-03 19:21:17 +0300

    New dialogSystem

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -5,67 +5,11 @@ using UnityEngine;
 using TMPro;
 
 [System.Serializable]
-public class Dialogue : MonoBehaviour
+public class Dialogue
 {
-    public TextMeshProUGUI textComponent;
-    public string name;
-    [TextArea(1,3)]
-    public string[] sometext;
-    public float textSpeed;
+    public string Name;
+    // public Sprite Portrait;
+    [TextArea(1,6)] //limit of text Area
+    public string[] Sentences;
 
-    public int index;
-    // Start is called before the first frame update
-    void Start()
-    {
-        textComponent.text = String.Empty;
-        TriggerDialogAccept();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (textComponent.text == sometext[index])
-            {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                textComponent.text = sometext[index];
-            }
-        }
-    }
-
-    void TriggerDialogAccept()
-    {
-        index = 0;
-        FindObjectOfType<DialogueManager>().StartDialogue(this);
-        // Typeline();
-    }
-
-    public void NextLine()
-    {
-        if (index < sometext.Length - 1)
-        {
-            index++;
-            textComponent.text = String.Empty;
-            StartCoroutine(Typeline());
-        }
-        else
-        {
-            gameObject.SetActive(false);
-        }
-    }
-
-
-    public IEnumerator Typeline()
-    {
-        foreach (char symbol in sometext[index].ToCharArray())
-        {
-            textComponent.text += symbol;
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
 }
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -2,33 +2,54 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-    private Queue<string> sentences;
-    // Start is called before the first frame update
+    private Queue<string> _sentences;
+    public Text NameText;
+    public Text DialogueSentences;
+    // public GameObject Portrait;
+
     void Start()
     {
-        sentences = new Queue<string>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _sentences = new Queue<string>();
     }
 
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log($"Start dialogue with {dialogue.name}");
-        
-        sentences.Clear();
-        dialogue.Typeline();
+        Debug.Log($"Start a conversation with {dialogue.Name}.");
+        NameText.text = dialogue.Name;
+        // Portrait = dialogue.Portrait;
+        _sentences.Clear();
+
+        foreach (var text in dialogue.Sentences) //Can upgrade wih second foreach to do a load of letters, keep it in mind
+        {
+            _sentences.Enqueue(text);
+            
+        }
+        DisplayNextSentences();
     }
-    
-    public void NextLine(Dialogue dialogue)
+
+    public void DisplayNextSentences() //Add many ways to continue a dialogue
     {
-        
+        if(_sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter) ||
+            Input.GetMouseButtonDown(0)) // space,enter,left click
+        {
+            string sentence = _sentences.Dequeue();
+            DialogueSentences.text = sentence;
+            Debug.Log(sentence);
+        }
+    }
+
+    private void EndDialogue()
+    {
+        Debug.Log("End of conversation");
     }
 }
