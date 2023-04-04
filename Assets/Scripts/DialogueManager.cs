708bb344940f7c9e95aca3f36eee605dfeb1542e
DekiruDeku <mikereaper@inbxo.ru>
2023-04-04 19:53:39 +0300

    the main scheme of dialog ready

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
     private Queue<string> _sentences;
-    public Text NameText;
-    public Text DialogueSentences;
+    public Text NameTextFieldInCanvas;
+    public Text TextFieldInCanvas;
+
+    public Animator animator;
     // public GameObject Portrait;
 
     void Start()
@@ -19,9 +22,11 @@ public class DialogueManager : MonoBehaviour
     public void StartDialogue(Dialogue dialogue)
     {
         Debug.Log($"Start a conversation with {dialogue.Name}.");
-        NameText.text = dialogue.Name;
+        NameTextFieldInCanvas.text = dialogue.Name;
         // Portrait = dialogue.Portrait;
         _sentences.Clear();
+
+        animator.SetBool("IsOpen", true);
 
         foreach (var text in dialogue.Sentences) //Can upgrade wih second foreach to do a load of letters, keep it in mind
         {
@@ -39,17 +44,33 @@ public class DialogueManager : MonoBehaviour
             return;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter) ||
-            Input.GetMouseButtonDown(0)) // space,enter,left click
+        string sentence;
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter)) // space,enter
         {
-            string sentence = _sentences.Dequeue();
-            DialogueSentences.text = sentence;
+            sentence = _sentences.Dequeue();
+            TextFieldInCanvas.text = sentence;
             Debug.Log(sentence);
+        }
+        //if hit a button
+        sentence = _sentences.Dequeue();
+        StopAllCoroutines(); //this need if we need to the next sentence this stop previosuly
+        StartCoroutine(TypeSentence(sentence));
+        Debug.Log(sentence);
+    }
+
+    private IEnumerator TypeSentence(string sentence)
+    {
+        TextFieldInCanvas.text = "";
+        foreach (var letters in sentence.ToCharArray())
+        {
+            TextFieldInCanvas.text += letters;
+            yield return null; //time to write a symbol
         }
     }
 
     private void EndDialogue()
     {
+        animator.SetBool("IsOpen", false);
         Debug.Log("End of conversation");
     }
 }
